cb348070ac24870ce5f2fcaa7197761f44fb3d1d
Javier <Javier@gmail.com>
2025-11-21 16:53:48 +0100

    comienzo

--- app.mjs
+++ app.mjs
@@ -1,7 +1,9 @@
 import { loadSequelize } from "./database.mjs";
-import express from "express";
+import express, { urlencoded } from "express";
 import cors from "cors";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
 
 /**
  * Point d'entrée de l'application
@@ -9,10 +11,79 @@ import bcrypt from "bcrypt";
  */
 async function main() {
     try {
-        const sequelize = await loadSequelize();
         const app = express();
+        app.use(express.json());
+        app.use(cookieParser());
+        const sequelize = await loadSequelize();
+        //console.log(sequelize.models)
+        const User = sequelize.models.User;
+        const Post = sequelize.models.Post;
+        const Comment = sequelize.models.Comment;
+        const JWT_SECRET = "secret";
+
+        app.post("/register", async (req, res) => {
+
+            try {
+                const { username, email, password } = req.body;
+
+                if (!username || !email || !password) {
+
+                    return res.status(400).json({ Message: 'Username, email and password are required' })
+                }
+
+                const user = await User.findOne({ where: { email: email } });
+
+                if (user !== null) {
+
+                    return res.status(400).json({ Message: 'Account already exists' })
+                }
+
+                const newUser = await User.create({
+                    username,
+                    email,
+                    password
+                });
+                const token = jwt.sign(
+                    { userId: newUser.id },
+                    JWT_SECRET,
+                    { expiresIn: '1h' });
+
+                if (jwt.verify(token, JWT_SECRET)) {
+                    console.log(token)
+                } else {
+                    console.log(error)
+                }
+
+                res.cookie('token', token, { httpOnly:true });
+                res.json({
+                    Message: 'successfully registered',
+                    User: newUser
+                });
 
 
+            } catch (error) {
+                console.log(error)
+                return res.status(400).json({ Message: 'Registration failed, please try again' })
+            }
+        });
+
+        /*
+        app.post("/login", async(req,res)=>{
+
+            
+            
+        });
+        
+        app.get();
+        app.get();
+        
+        app.post();
+        app.post();
+        app.post();
+        app.delete();
+        app.delete();
+        
+        */
 
 
 
--- database.mjs
+++ database.mjs
@@ -1,19 +1,85 @@
 import { Sequelize, DataTypes } from "sequelize";
+import bcrypt from "bcrypt";
 
 /**
  * 
  * @returns {Promise<Sequelize>}
  */
 export async function loadSequelize() {
+
     try {
-        const sequelize = new Sequelize(/* ... */);
+        const sequelize = new Sequelize(
+            'api-tread',
+            'root',
+            'root',
+            {
+                host: '127.0.0.1',
+                dialect: 'mysql'
+            }
+
+        );
+
         // ...
+        sequelize.define("User",
+            {
+                username: DataTypes.STRING,
+
+                email: {
+                    type: DataTypes.STRING,
+                    unique: true,
+                    validate: {
+                        isEmail: true,
+
+                    }
+                },
+
+                password: {
+
+                    type: DataTypes.STRING,
+
+                    set(clearPassword) {
+
+                        const hashPassword = bcrypt.hashSync(clearPassword, 10);
+                        this.setDataValue('password', hashPassword);
+                    }
+
+                }
+
+            });
+
+        sequelize.define("Post",
+            {
+                title: DataTypes.STRING,
+                content: DataTypes.STRING,
+            });
+
+        sequelize.define("Comment",
+            {
+                content: DataTypes.STRING
+            });
+
+        await sequelize.sync({ force: true });
+
+        const User = sequelize.models.User;
+        const Post = sequelize.models.Post;
+        const Comment = sequelize.models.Comment;
+
+        User.hasMany(Post);
+        Post.belongsTo(User);
+
+        User.hasMany(Comment);
+        Comment.belongsTo(User);
+
+        Post.hasMany(Comment);
+        Comment.belongsTo(Post);
+
+
         return sequelize;
+
     } catch (error) {
+
         console.error(error);
         throw Error("Échec du chargement de Sequelize");
-    }
-
-    // ...
+    };
 
 }
